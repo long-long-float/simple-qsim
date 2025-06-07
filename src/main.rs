72ccae6be77d40db87ff69bfceb9c6799c99492b
long-long-float <niinikazuki@yahoo.co.jp>
2025-06-07 16:38:26 +0900

    Add Quantum Fourier Transform

--- src/main.rs
+++ src/main.rs
@@ -42,11 +42,44 @@ fn build_x_matrix() -> CsrMatrix<Qbit> {
     CsrMatrix::from(&x_coo)
 }
 
+fn build_z_matrix() -> CsrMatrix<Qbit> {
+    let mut z_coo = CooMatrix::new(2, 2);
+    z_coo.push(0, 0, Complex::new(1.0, 0.0));
+    z_coo.push(1, 1, Complex::new(-1.0, 0.0));
+    CsrMatrix::from(&z_coo)
+}
+
+fn build_s_matrix() -> CsrMatrix<Qbit> {
+    let mut s_coo = CooMatrix::new(2, 2);
+    s_coo.push(0, 0, Complex::new(1.0, 0.0));
+    s_coo.push(1, 1, Complex::new(0.0, 1.0));
+    CsrMatrix::from(&s_coo)
+}
+
+fn build_t_matrix() -> CsrMatrix<Qbit> {
+    let mut t_coo = CooMatrix::new(2, 2);
+    t_coo.push(0, 0, Complex::new(1.0, 0.0));
+    t_coo.push(1, 1, Complex::from_polar(1.0, std::f64::consts::FRAC_PI_4));
+    CsrMatrix::from(&t_coo)
+}
+
 struct QState {
     state: DVector<Qbit>,
 }
 
 impl QState {
+    fn new(state: &[Qbit]) -> Result<Self> {
+        let len = state.len();
+        if len == 0 || (len & (len - 1)) != 0 {
+            return Err(anyhow::anyhow!(
+                "State vector length must be a non-zero power of 2"
+            ));
+        }
+
+        let state = DVector::from_row_slice(state);
+        Ok(Self { state })
+    }
+
     fn from_str(qbits: &str) -> Result<Self> {
         let index = usize::from_str_radix(qbits, 2)?;
         let mut state = DVector::zeros(2_usize.pow(qbits.len() as u32));
@@ -95,16 +128,13 @@ impl Circuit {
         Ok(self.num_of_qbits - 1 - index)
     }
 
-    #[allow(non_snake_case)]
-    fn H(mut self, index: usize) -> Result<Self> {
+    fn add_gate_at(mut self, index: usize, gate: CsrMatrix<Qbit>) -> Result<Self> {
         let index = self.check_and_revsere_index(index)?;
-
-        let h = build_hadamard_matrix();
 
         let mut matrix = CsrMatrix::identity(1);
         for i in 0..self.num_of_qbits {
             if i == index {
-                matrix = tensor_product(&matrix, &h);
+                matrix = tensor_product(&matrix, &gate);
             } else {
                 matrix = tensor_product(&matrix, &CsrMatrix::identity(2));
             }
@@ -112,6 +142,11 @@ impl Circuit {
 
         self.add_gate(matrix);
         Ok(self)
+    }
+
+    #[allow(non_snake_case)]
+    fn H(self, index: usize) -> Result<Self> {
+        self.add_gate_at(index, build_hadamard_matrix())
     }
 
     fn control(mut self, control: usize, target: usize, gate: &CsrMatrix<Qbit>) -> Result<Self> {
@@ -160,6 +195,19 @@ impl Circuit {
         self.control(control, target, &build_x_matrix())
     }
 
+    fn swap(self, index1: usize, index2: usize) -> Result<Self> {
+        let index1 = self.check_and_revsere_index(index1)?;
+        let index2 = self.check_and_revsere_index(index2)?;
+
+        if index1 == index2 {
+            return Err(anyhow::anyhow!("Cannot swap a qubit with itself"));
+        }
+
+        self.cnot(index1, index2)?
+            .cnot(index2, index1)?
+            .cnot(index1, index2)
+    }
+
     fn add_gate(&mut self, gate: CsrMatrix<Qbit>) {
         self.gates.push(gate);
     }
@@ -189,13 +237,24 @@ fn main() -> Result<()> {
         .apply(&q00);
     println!("{}", result);
 
+    // Quantum Fourier Transform
+    let qstate = QState::new(&[Complex::new(1.0, 0.0) / 8.0_f64.sqrt(); 8])?;
+    println!("{}", qstate);
+
+    let qft = Circuit::new(qstate.len())
+        // First bit
+        .H(0)?
+        .control(1, 0, &build_s_matrix())?
+        .control(2, 0, &build_t_matrix())?
+        // Second bit
+        .H(1)?
+        .control(2, 1, &build_s_matrix())?
+        // Third bit
+        .H(2)?
+        .swap(0, 2)?;
+
+    let result = qft.apply(&qstate);
+    println!("{}", result);
+
     Ok(())
-
-    /*
-    let result5 = &cnot01 * (&hadamard0 * &q00);
-    println!("{:}", convert_csr_dense(&result5));
-
-    let result6 = &hadamard0 * (&ctrl_h01 * (&hadamard0 * &q00));
-    println!("{:}", convert_csr_dense(&result6));
-    */
 }
